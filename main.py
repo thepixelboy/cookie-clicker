5924c9226ae545f800c8bec4bcbb3b94721ad403
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-25 19:52:16 +0200

    feat: get cookie to click on

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+import time
 
 chrome_driver_path = "/Users/ruben/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element_by_id("cookie")
